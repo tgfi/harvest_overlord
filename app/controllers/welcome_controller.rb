ecd2728b181326f6b7597fe381cbc5d39d2fbb19
Greg Benedict <gbenedict@tgfi.net>
2014-01-21 07:27:08 -0500

    refactoring to list all users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,32 +1,24 @@
 class WelcomeController < ApplicationController
 
   def index
-
     @day = Chronic.parse(params[:date]) || previous_day
+    @users = Harvest::Application::CONFIG['user_emails'].to_s.gsub(' ', '').split(',').inject({}) { |h, elem| h[elem] = nil; h}
 
     harvest = Harvest.client( Harvest::Application::CONFIG['harvest']['subdomain'],  Harvest::Application::CONFIG['harvest']['login_email'],  Harvest::Application::CONFIG['harvest']['password'])
-    # harvest.projects.all   # list out projects
 
-    # client = Harvest::Client.new(:name => "Billable Company LTD.")
-    # client = harvest.clients.create(client)
-    # harvest.clients.find(client.id) # returns a Harvest::Client
+    if @users.empty?
+      harvest.users.all.each do |user|
+        @users[u.email] = Person.new(info: user, times: harvest.time.all(@day, user.id))
+      end
+    else
+      all_harvest_users = harvest.users.all
 
-    @users = Harvest::Application::CONFIG['user_emails'].to_s.gsub(' ', '').split(',').inject({}) { |h, elem| h[elem] = nil; h}
-     # {"mgilbank@tgfi.net" => nil, "msellers@tgfi.net" => nil, "mretzak@tgfi.net" => nil, "gbenedict@tgfi.net" => nil}
-
-    # get all users
-    h_users = harvest.users.all
-
-    # update our local hash
-    @users.each do |k,v|
-      p = Person.new()
-      p.info = h_users.select{|u| u.email == k }.first
-      # raise p
-      p.times = harvest.time.all(@day, p.info.id)
-
-      @users[k] = p
+      @users.each do |k,v|
+        all_harvest_users.select{|user| user.email == k }.each do |user|
+          @users[k] = Person.new(info: user, times: harvest.time.all(@day, user.id))
+        end
+      end
     end
-
   end
 
   private
